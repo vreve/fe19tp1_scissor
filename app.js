db8f2c9483abfd06b6de4701cf914b28a21f09f9
claesphilip <claesphiliplofgren@hotmail.com>
2019-11-01 11:56:14 +0100

    messega

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 
+// let user = document.getElementById(infoText);
+
+// function changeColor(newColor) {
+//     var elem = document.getElementById('infoText');
+//     elem.style.color = newColor;
+//   }
+
+//   localStorage.setItem('age', "30");
+
+//   var cat = localStorage.getItem('30');
+
+
+
+
+
