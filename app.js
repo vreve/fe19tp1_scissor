7c94ba81e603494642d8a9c0ba891f618e5b64a6
AddeFreak <andreas.frick.toresand@hotmail.com>
2019-11-06 14:33:48 +0100

    inforuta

--- app.js
+++ app.js
@@ -8,9 +8,20 @@
 
 //   localStorage.setItem('age', "30");
 
-//   var cat = localStorage.getItem('30');
+//   var cat = localStorage.getItem('30');/*  */
+
+window.addEventListener('DOMContentLoaded', myFunction);
 
 
+function myFunction() {
+    //console.log("yo");
+
+    if (!localStorage.getItem("alertdisplayed")) {
+        document.getElementById("myDiv").classList.toggle("nohidden");
+        //localStorage["alertdisplayed"] = true
+        localStorage.setItem('alertdisplayed', true);
+    }
+}
 
 
 
