57f1496b62e5e40d7338d0cf2971ecbab1bf5fae
Isabelle-123 <isabelle.caspersson@student.kyh.se>
2019-11-11 11:31:56 +0100

    med Kristians uppdateringar

--- app.js
+++ app.js
@@ -1,46 +1,43 @@
+var noteList = [];
 
 window.addEventListener('DOMContentLoaded', popUpLoad);
+  function closePopUp() {
+    let popup = document.querySelector("#popUp")
+    popup.style.display = "none"
+    localStorage.setItem('savePopUp', true);
+  }
 
 function popUpLoad() {
-
-    if (!localStorage.getItem('savePopUp')) {
-        document.getElementById('popUp').classList.toggle('showPopUp');
-        //localStorage["alertdisplayed"] = true
-        localStorage.setItem('savePopUp', true);
-    }
-}
-/*
-function removePopUpOnHide() {
-  if (localStorage.getItem('savePopUp', true)) {
-    document.setElementById('popUpNone'); 
-  } else {
+  if (!localStorage.getItem('savePopUp')) {
     document.getElementById('popUp').classList.toggle('showPopUp');
-    }
+    //localStorage["alertdisplayed"] = true
+    //localStorage.setItem('savePopUp', true);
   }
-  */
- 
+  renderNoteList();
+}
+
 var Delta = Quill.import('delta');
 var quill = new Quill('#editor-container', {
-    modules: {
-      toolbar: [
-        [{ header: [1, 2, false] }],
-        ['bold', 'italic', 'underline'],
-        [{ list : "ordered" }, { list : "bullet"}],
-        ['image', 'code-block']
-      ]
-    },
-    placeholder: 'Ny anteckning...',
-    theme: 'snow'  // or 'bubble'
-  });
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ['image', 'code-block']
+    ]
+  },
+  placeholder: 'Ny anteckning...',
+  theme: 'snow'  // or 'bubble'
+});
 
 // Store accumulated changes
 var change = new Delta();
-quill.on('text-change', function(delta) {
+quill.on('text-change', function (delta) {
   change = change.compose(delta);
 });
 
 // Save periodically
-setInterval(function() {
+setInterval(function () {
   if (change.length() > 0) {
     console.log('Saving changes', change);
     // Save the entire updated text to localStorage
@@ -48,19 +45,14 @@ setInterval(function() {
     localStorage.setItem('storedText', data);
     change = new Delta();
   }
-}, 5*1000);
+}, 5 * 1000);
 
 // Check for unsaved data
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
   if (change.length() > 0) {
     return 'There are unsaved changes. Are you sure you want to leave?';
   }
-} 
-
-
-
-
-
+}
 
 
 
@@ -73,9 +65,109 @@ const notes = document.querySelector('#notes');
 
 form.noteText.focus();
 
-// Functions
+function saveNotes () {
+  localStorage.setItem('notes', JSON.stringify(noteList))
+
+}
+
 function addNote() {
-  let text = form.value;
+  let note = {
+    id: Date.now(),
+    content: quill.getContents(),
+    title: quill.getText(0, 20),
+    favourite: false,
+  }
+
+  if (!noteList) {
+    noteList = [];
+  }
+  
+  noteList.push(note);
+  saveNotes();
+
+}
+function selectNote(noteID) {
+    console.log("id: " + noteID)
+    // hitta en note i noteList vars ID stämmer överrens med argumentet id.
+    // setcontents note.content
+    let note = noteList.find(note => note.id === noteID)
+    quill.setContents(note.content);
+  } 
+
+function renderDiv(note) {
+    let noteDivs = document.querySelector("#notes");
+    let myDiv = document.createElement('div');
+    let deleteButton = document.createElement('span');
+
+    myDiv.classList.add('note');
+    myDiv.id = note.id;
+    //color sparas inte på note:n
+    myDiv.classList.add(form.color.value);
+    myDiv.innerHTML = `${note.title} ${new Date(note.id).toLocaleTimeString()}`
+    myDiv.addEventListener("click", () => selectNote(note.id));
+    
+     //delete funkar ej
+    deleteButton.classList.add('note-delete');
+    deleteButton.innerHTML = '&times;';
+
+  
+
+    noteDivs.appendChild(myDiv);
+    myDiv.appendChild(deleteButton);
+    
+    form.noteText.value = '';
+    form.noteText.focus();
+
+    addListenerDeleteButton(deleteButton);
+  }
+
+  
+
+  //delete funkar ej
+  //göra       removeItem(note.id) ?
+  function addListenerDeleteButton(deleteButton) {
+    deleteButton.addEventListener('click', function (e) {
+      e.stopPropagation();
+      deleteNote(e);
+    });
+  }
+  
+  /*
+  function deleteNote(e) {
+    let clickedID = e.target.querySelector('span').innerHTML;
+    console.log(clickedID);
+    noteList = noteList.filter(item => item.id !== Number(clickedID));
+    e.target.remove();
+  }
+*/
+
+  function deleteNote(e) {
+    let noteID = e.target.parentNode.id
+    noteList = noteList.filter(note => note.id !== Number(noteID)); 
+
+    saveNotes();
+    let eventNote = e.target.parentNode;
+    eventNote.parentNode.removeChild(eventNote);
+  }
+
+
+function renderNoteList() {
+    document.querySelector("#notes").innerHTML = "";
+    noteList = JSON.parse(localStorage.getItem('notes'));
+    console.log(noteList)
+  /*
+  noteList.forEach(function (note) {
+    ...
+  }); */
+    noteList.forEach(note => {
+    renderDiv(note);
+  })
+
+}
+// Functions
+/*
+function oldaddNote() {
+  let text = quill.getText(0, 20);
   let note = document.createElement('div');
   let deleteButton = document.createElement('span');
 
@@ -85,31 +177,21 @@ function addNote() {
   deleteButton.classList.add('note-delete');
   deleteButton.innerHTML = '&times;';
 
-  note.appendChild(deleteButton);  
+  note.appendChild(deleteButton);
   notes.appendChild(note);
 
   form.noteText.value = '';
   form.noteText.focus();
 
   addListenerDeleteButton(deleteButton);
-}
+}*/
 
-function addListenerDeleteButton(deleteButton) {
-  deleteButton.addEventListener('click', function (e) {
-    e.stopPropagation();      
-    deleteNote(e);
-  });
-}
 
-function deleteNote(e) {
-  let eventNote = e.target.parentNode;
-  eventNote.parentNode.removeChild(eventNote);
-}
 
 
 // Event Listeners
 form.addButton.addEventListener('click', function (e) {
-  e.preventDefault();  
+  e.preventDefault();
   if (form.value != '') {
     addNote();
   }
